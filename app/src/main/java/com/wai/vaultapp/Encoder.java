4eaceb4ad3c7de359548435f11105582a73fa9f2
vampiremoe <56422331+vampiremoe@users.noreply.github.com>
2025-11-06 16:15:22 +0700

    Update Encoder.java

--- app/src/main/java/com/wai/vaultapp/Encoder.java
+++ app/src/main/java/com/wai/vaultapp/Encoder.java
@@ -3,63 +3,46 @@ package com.wai.vaultapp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class Encoder {
 
-    /**
-     * Encrypts a file using simple XOR mirroring like Decoder.
-     * @param inputFile File to encrypt
-     * @param outputBaseFolder Base folder for encrypted files
-     * @param key XOR key
-     * @return true if successful
-     */
-    public boolean encodeFile(File inputFile, String outputBaseFolder, int key) {
-        if(!inputFile.exists()) return false;
+    public boolean encodeFile(File inputFile, String baseOutputFolder, int key){
+        try {
+            String ext = getExtension(inputFile.getName());
+            File saveFolder = new File(baseOutputFolder, getFolderByType(ext));
+            if(!saveFolder.exists()) saveFolder.mkdirs();
 
-        String ext = getFileExtension(inputFile.getName());
-        String typeFolder = "others";
-        if(ext.equalsIgnoreCase(".jpg") || ext.equalsIgnoreCase(".png") || ext.equalsIgnoreCase(".gif") || ext.equalsIgnoreCase(".svg") || ext.equalsIgnoreCase(".webp")) {
-            typeFolder = "images";
-        } else if(ext.equalsIgnoreCase(".mp4") || ext.equalsIgnoreCase(".mkv") || ext.equalsIgnoreCase(".mov") || ext.equalsIgnoreCase(".3gp") || ext.equalsIgnoreCase(".wmv") || ext.equalsIgnoreCase(".avi") || ext.equalsIgnoreCase(".flv") || ext.equalsIgnoreCase(".m4v") || ext.equalsIgnoreCase(".vob")) {
-            typeFolder = "videos";
-        }
-
-        File saveDir = new File(outputBaseFolder + "/encrypt", typeFolder);
-        if(!saveDir.exists()) saveDir.mkdirs();
-
-        File saveFile = new File(saveDir, inputFile.getName() + ".bin");
-        for(int i=0; saveFile.exists(); i++){
-            saveFile = new File(saveDir, "(" + i + ") " + inputFile.getName() + ".bin");
-        }
-
-        try (FileInputStream fis = new FileInputStream(inputFile);
-             FileOutputStream fos = new FileOutputStream(saveFile)) {
-
-            byte[] buffer = new byte[128];
-            int readLen = fis.read(buffer);
-            for(int i=0; i<readLen; i++){
-                buffer[i] = (byte)(buffer[i]^key);
-            }
-            fos.write(buffer,0,readLen);
-
-            // Write remaining
-            buffer = new byte[262144];
-            int len;
-            while((len = fis.read(buffer)) > 0){
-                fos.write(buffer,0,len);
+            File outputFile = new File(saveFolder,inputFile.getName());
+            for(int i=0; outputFile.exists(); i++){
+                outputFile = new File(saveFolder,"("+i+") "+inputFile.getName());
             }
 
-        } catch (IOException e){
-            e.printStackTrace();
-            return false;
-        }
-        return true;
+            FileInputStream fis = new FileInputStream(inputFile);
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            byte[] buffer = new byte[1024];
+            int read;
+            while((read=fis.read(buffer))!=-1){
+                for(int i=0;i<read;i++){
+                    buffer[i] = (byte)(buffer[i]^key);
+                }
+                fos.write(buffer,0,read);
+            }
+            fis.close();
+            fos.close();
+            return true;
+        } catch(Exception e){ e.printStackTrace(); return false; }
     }
 
-    private String getFileExtension(String name){
-        int lastDot = name.lastIndexOf(".");
-        if(lastDot>0) return name.substring(lastDot);
-        return "";
+    private String getFolderByType(String ext){
+        ext = ext.toLowerCase();
+        if(ext.contains("jpg")||ext.contains("png")||ext.contains("gif")||ext.contains("svg")||ext.contains("webp")) return "images";
+        if(ext.contains("mp4")||ext.contains("3gp")||ext.contains("mkv")||ext.contains("mov")||ext.contains("avi")||ext.contains("wmv")||ext.contains("flv")||ext.contains("vob")||ext.contains("webm")||ext.contains("m4v")) return "videos";
+        return "others";
+    }
+
+    private String getExtension(String name){
+        int idx = name.lastIndexOf('.');
+        if(idx==-1) return "";
+        return name.substring(idx+1);
     }
 }
